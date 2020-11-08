bdcdacd9baafe070988b728c23a5e8de29223005
The-Bowman <72958677+The-Bowman@users.noreply.github.com>
2020-11-07 22:37:49 -0700

    Add project files.

---
+++ Milestone2/InventoryItem.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Milestone2
+{
+    class InventoryItem
+    {
+        private string itemName;
+        private string itemDescr;
+        private int itemNum;
+        private double price;
+
+        //Constructor methods
+        public InventoryItem()
+        {
+            itemName = "Unknown";
+            itemDescr = "Unknown";
+            itemNum = 999;
+            price = 999.99;
+        }
+
+        public InventoryItem(string itemName, string itemDescr, int itemNum, double price)
+        {
+            this.itemName = itemName;
+            this.itemDescr = itemDescr;
+            this.itemNum = itemNum;
+            this.price = price;
+        }
+
+
+        //getter and setter methods for variables
+        public int getItemNum()
+        {
+            return itemNum;
+        }
+
+        public void setItemNum(int itemNum)
+        {
+            this.itemNum = itemNum;
+        }
+
+        public double getPrice()
+        {
+            return price;
+        }
+
+        public void setPrice(double price)
+        {
+            this.price = price;
+        }
+
+        public string getItemName()
+        {
+            return itemName;
+        }
+
+        public void setItemName(string itemName)
+        {
+            this.itemName = itemName;
+        }
+
+        public string getDescr()
+        {
+            return itemDescr;
+        }
+
+        public void setDescr(string itemDescr)
+        {
+            this.itemDescr = itemDescr;
+        }
+
+
+    }
+}
---
+++ Milestone2/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Milestone2
+{
+    class Driver
+    {
+        private static List<InventoryItem> items = new List<InventoryItem>();
+        static void Main(String[] args)
+        {
+            InventoryItem item = new InventoryItem();
+            item.getPrice();
+            item.getItemNum();
+            Console.WriteLine(item.getItemName());
+            item = new InventoryItem("Silver Nonsense Scribbler", "A pen in a silver chassis.", 555618, 19.99);
+
+            items.Add(item);
+            Console.WriteLine (items[0].getItemName());
+        }
+    }
+}
